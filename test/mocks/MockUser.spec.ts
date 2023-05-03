78d29f367853edc6d4084330848ade738d7eb091
Naomi Carrigan <nhcarrigan@gmail.com>
2023-05-03 13:54:10 -0700

    wip: stub mock user tests as class is reasonably complete

---
+++ test/mocks/MockUser.spec.ts
@@ -0,0 +1,51 @@
+import { assert } from "chai";
+
+suite("Mock User", () => {
+  test("should instantiate", () => {
+    assert.fail();
+  });
+
+  test("should get ID", () => {
+    assert.fail();
+  });
+
+  test("should get username", () => {
+    assert.fail();
+  });
+
+  test("should get tag", () => {
+    assert.fail();
+  });
+
+  test("should get discriminator", () => {
+    assert.fail();
+  });
+
+  test("should get avatar", () => {
+    assert.fail();
+  });
+
+  test("should get whether the user is a bot", () => {
+    assert.fail();
+  });
+
+  test("should get whether the user is a system user", () => {
+    assert.fail();
+  });
+
+  test("should get list of sent messages", () => {
+    assert.fail();
+  });
+
+  test("should fetch user", () => {
+    assert.fail();
+  });
+
+  test("should display avatar URL", () => {
+    assert.fail();
+  });
+
+  test("should send message", () => {
+    assert.fail();
+  });
+});
