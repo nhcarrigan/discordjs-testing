53d50fae6a1d1de7a0e8fa57856c2034bec0f51d
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-30 16:15:50 -0700

    feat: tests for utils

--- test/utils/Snowflake.spec.ts
+++ test/utils/Snowflake.spec.ts
@@ -2,8 +2,28 @@ import { assert } from "chai";
 
 import { Snowflake } from "../../src/utils/Snowflake";
 
-suite("this is an example", () => {
-  test("using assert", () => {
-    assert.isDefined(Snowflake);
+suite("Snowflake", () => {
+  /**
+   * Instantiation.
+   */
+
+  test("should instantiate", () => {
+    const flake = new Snowflake();
+    assert.exists(flake);
+    assert.instanceOf(flake, Snowflake);
+  });
+
+  /**
+   * Properties.
+   */
+
+  /**
+   * Methods.
+   */
+
+  test("should generate a string snowflake", () => {
+    const flake = new Snowflake();
+    assert.exists(flake.id);
+    assert.isString(flake.id);
   });
 });
--- test/utils/logHandler.spec.ts
+++ test/utils/logHandler.spec.ts
@@ -2,8 +2,23 @@ import { assert } from "chai";
 
 import { logHandler } from "../../src/utils/logHandler";
 
-suite("this is an example", () => {
-  test("using assert", () => {
+suite("logHandler", () => {
+  /**
+   * Instantiation.
+   */
+  test("should be defined", () => {
     assert.isDefined(logHandler);
+  });
+  /**
+   * Properties.
+   */
+
+  /**
+   * Methods.
+   */
+  test("should be able to log", () => {
+    assert.property(logHandler, "log");
+    assert.isFunction(logHandler.log);
+    assert.doesNotThrow(() => logHandler.log("info", "test"));
   });
 });
